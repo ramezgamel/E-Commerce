52cd31d453ab8b4af7092c46d4904984dd141588
ramezgamel <98866573+ramezgamel@users.noreply.github.com>
2023-10-02 00:18:46 -0700

    Update apiSlice.js

--- frontend/src/store/apiSlice.js
+++ frontend/src/store/apiSlice.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "/api/",
+    baseUrl: "https://ramez-proshop.onrender.com/api",
+    credentials: "include",
   }),
   tagTypes: ["Product", "Order", "User"],
   endpoints: (builder) => ({}),
