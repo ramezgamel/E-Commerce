46958bf95ba6a9336c9cd2c2b8752ce778c54e93
ramez <ramezgamel52@gmail.com>
2023-10-15 12:05:03 +0300

    fix bug

--- frontend/src/store/apiSlice.js
+++ frontend/src/store/apiSlice.js
@@ -1,14 +1,14 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://ramez-proshop.onrender.com/api",
-    baseUrl: "http://localhost:5000/api",
-    credentials: "include",
+    baseUrl: `${import.meta.env.VITE_BASE_URL}/api`,
+    // baseUrl: "http://localhost:5000/api",
+    credentials: 'include',
     prepareHeaders: (Headers) => {
       return Headers;
     },
   }),
-  tagTypes: ["Product", "Order", "User"],
+  tagTypes: ['Product', 'Order', 'User'],
   endpoints: (builder) => ({}),
 });
