1b34971fd3d9d9bb310bc4a22ea7ec4637885cb3
ramez <ramezgamel52@gmail.com>
2024-06-12 19:16:14 +0300

    change tabs style

--- frontend/src/components/Tabs.jsx
+++ frontend/src/components/Tabs.jsx
@@ -3,8 +3,8 @@ import { useGetCatsQuery } from "../store/catApiSlice";
 function Tabs({ setCategory, category }) {
   const { data } = useGetCatsQuery(1);
   return (
-    <div className="w-full pt-1 font-medium border-b ">
-      <ul className="flex flex-wrap -mb-px">
+    <div className="w-full flex pt-1 font-medium border-b">
+      <ul className="flex -mb-px no-scrollbar overflow-scroll">
         <li
           onClick={() => setCategory("")}
           className={`${category == "" ? "border-blue-600 text-blue-600" : "text-main border-transparent"} mr-2 inline-block pb-2 px-3 border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-400 dark:hover:text-gray-400 hover:cursor-pointer`}>
